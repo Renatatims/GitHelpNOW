9807114e228d1419dccc2303916fc94452e6f6a1
ReTims <renatatims@gmail.com>
2022-09-14 23:47:03 -0500

    User's input value added var inputVar

--- assets/js/mapscript.js
+++ assets/js/mapscript.js
@@ -50,7 +50,14 @@ function initAutocomplete() {
     }
     // Create the search box and link it to the UI element.
     var input = document.getElementById("pac-input");
+
+    //************Input User's value*********************//
+    var inputVal = document.getElementById("pac-input").value;
+    console.log(inputVal);
+    //************Input User's value*********************//
+
     var searchBox = new google.maps.places.SearchBox(input);
+    console.log(searchBox);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     // Bias the SearchBox results towards current map's viewport.
     map.addListener("bounds_changed", function () {
@@ -61,6 +68,7 @@ function initAutocomplete() {
     // more details for that place.
     searchBox.addListener("places_changed", function () {
         var places = searchBox.getPlaces();
+        console.log(places);
         if (places.length == 0) {
             return;
         }
