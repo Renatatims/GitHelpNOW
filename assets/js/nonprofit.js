9e030345efad3d39484ac746b30a14b9d60e68a3
ReTims <renatatims@gmail.com>
2022-09-16 12:01:02 -0500

    Womenshelter file created + Function women shelter to diplay the organizations info

--- assets/js/nonprofit.js
+++ assets/js/nonprofit.js
@@ -1,5 +1,5 @@
 var APIkey = "aff405eca8c6c3b65de5c821a36553f7"
-
+   
 
 function foodBank(){
     var queryUrl = "https://partners.every.org/v0.2/search/foodbank?apiKey=aff405eca8c6c3b65de5c821a36553f7";
@@ -24,7 +24,7 @@ function foodBank(){
                 $("#imageOrg"+i).attr("src", imageOrgResult);
                 $("#logoOrg"+i).attr("src", logoOrgResult);
                 $("#visitBtn"+i).attr("href",profileUrl);
-                console.log(resultFacility);
+                //console.log(resultFacility);
               
             }
             
@@ -45,19 +45,19 @@ function shelter(){
 
                 for (i=0; i<11; i++){
                 
-                var nameShelterResult = resultFacility.nonprofits[i].name;
-                var descriptionShelterResult = resultFacility.nonprofits[i].description;
-                var imageShelterResult = resultFacility.nonprofits[i].coverImageUrl;
-                var logoShelterResult = resultFacility.nonprofits[i].logoUrl;
-                var profileUrlShelter = resultFacility.nonprofits[i].profileUrl
+                    var nameOrgResult = resultFacility.nonprofits[i].name;
+                    var descriptionOrgResult = resultFacility.nonprofits[i].description;
+                    var imageOrgResult = resultFacility.nonprofits[i].coverImageUrl;
+                    var logoOrgResult = resultFacility.nonprofits[i].logoUrl;
+                    var profileUrl = resultFacility.nonprofits[i].profileUrl
                 //var foodBankEl = document.getElementById("foodBank" + i);
  
-                $("#nameShelter"+i).text(nameShelterResult);
-                $("#descriptionShelter"+i).text(descriptionShelterResult);
-                $("#imageShelter"+i).attr("src", imageShelterResult);
-                $("#logoShelter"+i).attr("src", logoShelterResult);
-                $("#visitBtnShelter"+i).attr("href",profileUrlShelter);
-                console.log(resultFacility);
+                $("#nameShelter"+i).text(nameOrgResult);
+                $("#descriptionShelter"+i).text(descriptionOrgResult);
+                $("#imageShelter"+i).attr("src", imageOrgResult);
+                $("#logoShelter"+i).attr("src", logoOrgResult);
+                $("#visitBtnShelter"+i).attr("href",profileUrl);
+                //console.log(resultFacility);
               
             }
             
@@ -67,8 +67,8 @@ shelter();
 
 
 
-function shelter(){
-    var queryUrl = "https://partners.every.org/v0.2/search/house?apiKey=aff405eca8c6c3b65de5c821a36553f7";
+function womenShelter(){
+    var queryUrl = "https://partners.every.org/v0.2/search/Womenshelter?apiKey=aff405eca8c6c3b65de5c821a36553f7";
     fetch(queryUrl).then(function(facility){
         if(facility.ok){
             facility.json().then(function(result){
@@ -85,16 +85,17 @@ function shelter(){
                 var profileUrlShelter = resultFacility.nonprofits[i].profileUrl
                 //var foodBankEl = document.getElementById("foodBank" + i);
  
-                $("#nameShelter"+i).text(nameShelterResult);
-                $("#descriptionShelter"+i).text(descriptionShelterResult);
-                $("#imageShelter"+i).attr("src", imageShelterResult);
-                $("#logoShelter"+i).attr("src", logoShelterResult);
-                $("#visitBtnShelter"+i).attr("href",profileUrlShelter);
+                $("#nameWomen"+i).text(nameShelterResult);
+                $("#descriptionWomen"+i).text(descriptionShelterResult);
+                $("#logoWomen"+i).attr("src", logoShelterResult);
+                $("#visitBtnWomen"+i).attr("href",profileUrlShelter);
                 console.log(resultFacility);
               
             }
             
         })}})};
  
-shelter();
+womenShelter();
+
+
 
